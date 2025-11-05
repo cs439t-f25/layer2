f1714bea780456b0a2eabd6f072b4128489ba2de
utgheith <utgheith@users.noreply.github.com>
2025-11-05 10:12:15 -0600

    Listen

--- mgr.go
+++ mgr.go
@@ -9,11 +9,11 @@ package layer2
 // Why is it here? To keep the interface seperate from the implementation
 
 type Connection struct {
-	toNetwork   chan byte
-	fromNetwork chan byte
+	ToNetwork   chan byte
+	TromNetwork chan byte
 }
 
-type TestMgr interface {
+type Mgr interface {
 	// configure the network interface, can be called at most once
 	IfConfig(myMac *MacAddr, myName string) error
 
@@ -22,7 +22,12 @@ type TestMgr interface {
 	//     - implements a reliable, full-duplex byte stream
 	//     - uniquely identified by (port1, host1, port2, host2)
 	//
+	// notice that we pretend IP addresses and sockets don't exist
+	//
 	// fails if called before IfConfig
 	//
 	Connect(myPort int, otherPort int, otherName string) (*Connection, error)
+
+	// listen for for connections on the given port
+	Listen(myPort int) (*Connection, error)
 }
